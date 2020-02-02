557633401f84620d844a688b72047cefcdbed4c7
Erick Johnson <erick_johnson@live.com>
2020-02-02 14:37:59 -0300

    update mongoose version/ add summary

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost/my-money')
+module.exports = mongoose.connect('mongodb://localhost/mymoney', {
+  useNewUrlParser: true
+})
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
