3eb456dbd948a28df5b465f9db682eb9f36cc53e
Erick Johnson <erick_johnson@live.com>
2020-02-03 14:59:46 -0300

    add footer component / fix footer css

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2020
+      <a href="#" target="_blank">
+        &nbsp; EJ Softwares
+      </a>
+    </strong>
+  </footer>
+);
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import "../common/template/dependencies";
 import Header from "../common/template/header";
 import Sidebar from "../common/template/sidebar";
-
+import Footer from "../common/template/footer";
 export default props => (
   <div className="wrapper">
     <Header></Header>
     <Sidebar />
+    <div className="content-wrapper">
+      <h1>Conteudoo</h1>
+    </div>
+    <Footer />
   </div>
 );
