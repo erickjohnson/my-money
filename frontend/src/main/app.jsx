b322db7c3239b32c130c281b92b83ab4ba45cbfc
Erick Johnson <erick_johnson@live.com>
2020-02-03 17:28:01 -0300

    add routes

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,12 +3,14 @@ import "../common/template/dependencies";
 import Header from "../common/template/header";
 import Sidebar from "../common/template/sidebar";
 import Footer from "../common/template/footer";
+import Routes from "./routes";
+
 export default props => (
   <div className="wrapper">
     <Header></Header>
     <Sidebar />
     <div className="content-wrapper">
-      <h1>Conteudoo</h1>
+      <Routes />
     </div>
     <Footer />
   </div>
