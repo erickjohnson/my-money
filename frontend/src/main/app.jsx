87501673e5bada87cbdb645c71008ec60d70fd04
Erick Johnson <erick_johnson@live.com>
2020-02-03 13:56:35 -0300

    finish header (add toggle and layout)

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "../common/template/dependencies";
+import Header from "../common/template/header";
+
+export default props => (
+  <div className="wrapper">
+    <Header></Header>
+  </div>
+);
